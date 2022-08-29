22ffec6ebd4108d8e4e29bd2258f0585986943d2
fogg-uw <fogg@wisc.edu>
2022-08-29 15:40:41 -0500

    continuing to twiddle

--- 0_control_params.R
+++ 0_control_params.R
@@ -113,11 +113,11 @@ serial_job = function(i) {
   
   source(script3)
   
-  print(startingdir)
+  cat(startingdir)
   setwd(startingdir)
   jobdir = paste0("job", i)
   setwd(jobdir)
-  print(jobdir)
+  cat(jobdir)
   
   #command3 = paste(R,     "3_summarize_findings.R",           sep=" ")
   
